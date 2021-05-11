c59c0fcdfdf379e06bb22624d3cf52336a48a04a
Jamesanthyoung1 <62122658+Jamesanthyoung1@users.noreply.github.com>
2021-05-11 17:09:52 +1000

    More work on CLI
    Still need to fix error where menu displays twice when selecting an option.

--- Prototype/app.js
+++ Prototype/app.js
@@ -2,26 +2,18 @@ const chalk = require('chalk');
 const clear = require('clear');
 const figlet = require('figlet');
 const http = require('http');
+const prompt = require('prompt-sync')({sigint: true});
+
 const readline = require('readline').createInterface({
     input: process.stdin,
     output: process.stdout
 });
 
-
-var menuHandler;
+var answer;
 
 // Initialize
 function initialize() {
     showMain();
-    process.stdin.setEncoding('utf8');
-    process.stdin.on('readable', checkMenu);
-
-    function checkMenu() {
-        var input = process.stdin.read();
-        if(input !== null) {
-            menuHandler(input.trim());
-        }
-    }
 }
 
 function title(){
@@ -35,53 +27,108 @@ function title(){
 
 function showMain(){
     title();
+    answer = prompt('1. Start Component' + '\n' + 
+        '2. Stop Component' + '\n' + 
+        '3. Restart Component' + '\n' + 
+        '4. New Component' + '\n' + 
+        '5. Delete Component' + '\n' + 
+        'Please select a number\n');
+            switch(answer){
+                case '1':
+                    startComponent();
+                break;
+                case '2':
+                    stopComponent();
+                break;
+                case '3':
+                    answer = prompt('Which Component would you like to Restart?\n ');
+                break;
+                case '4':
+                        answer = prompt('What new Component would you like to start?\n');
+                break;
+                case '5':
+                        answer = prompt('Which Component would you like to Delete?\n ');
+                break;
+            };
+    }
+
+
+function startComponent(){
+    title();
+    answer = prompt('1. Start Container' + '\n' + 
+        '2. Start Instance' + '\n' + 
+        '3. Start Server' + '\n' + 
+        '4. Back to Main Menu' + '\n' + 
+        'Please select a number\n'); 
+            switch(answer){
+                case '1':
+                    answer = prompt('Feature in progress\n ');
+                break;
+                case '2':
+                    answer = prompt('Feature in progress\n ');
+                break;
+                case '3':
+                    answer = prompt('Feature in progress\n ');
+                break;
+                case '4':
+                    showMain();
+                break;
+                };
+    }
+
+
+    function stopComponent(){
+        title();
+        answer = prompt('Which Component would you like to stop?\n ' + 
+            '1. Stop Container' + '\n' + 
+            '2. Stop Instance' + '\n' + 
+            '3. Stop Server' + '\n' + 
+            '4. Back to Main Menu' + '\n' + 
+            'Please select a number\n'); 
+                switch(answer){
+                    case '1':
+                        answer = prompt('Feature in progress\n ');
+                    break;
+                    case '2':
+                        answer = prompt('Feature in progress\n ');
+                    break;
+                    case '3':
+                        answer = prompt('Feature in progress\n ');
+                    break;
+                    case '4':
+                        showMain();
+                    break;
+                    };
+        }
+
+function restartComponent(){
+    title();
     console.log('1. Start Component' + '\n' + 
         '2. Stop Component' + '\n' + 
         '3. Restart Component' + '\n' + 
         '4. New Component' + '\n' + 
         '5. Delete Component' + '\n' + 
         'Please select a number');
-
-        menuHandler = function(input){
-            switch(input){
-                case '1':
-                console.log(
-                    readline.question('Which Component would you like to start?\n ', answer => {
-                        readline.close();
-                    })
-                );
-                break;
-            case '2':
-                console.log(
-                    readline.question('Which Component would you like to stop?\n ', answer => {
-                        readline.close();
-                    })
-                );
-                break;
-            case '3':
-                console.log(
-                    readline.question('Which Component would you like to Restart?\n ', answer => {
-                        readline.close();
-                    })
-                );
-                break;
-            case '4':
-                console.log(
-                    readline.question('What new Component would you like to start?\n ', answer => {
-                        readline.close();
-                    })
-                );
-                break;
-            case '5':;
-                console.log(
-                    readline.question('Which Component would you like to Delete?\n ', answer => {
-                        readline.close();
-                    })
-                );
-                break;
-            }
-    }
 }
 
+function newComponent(){
+    title();
+    console.log('1. Start Component' + '\n' + 
+        '2. Stop Component' + '\n' + 
+        '3. Restart Component' + '\n' + 
+        '4. New Component' + '\n' + 
+        '5. Delete Component' + '\n' + 
+        'Please select a number');
+}
+
+function deleteComponent(){
+    title();
+    console.log('1. Start Component' + '\n' + 
+        '2. Stop Component' + '\n' + 
+        '3. Restart Component' + '\n' + 
+        '4. New Component' + '\n' + 
+        '5. Delete Component' + '\n' + 
+        'Please select a number');
+}
 
 initialize();
