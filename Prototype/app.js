a0fa471d010712d9601c6d41cf1eec56d89b694c
Jamesanthyoung1 <62122658+Jamesanthyoung1@users.noreply.github.com>
2021-05-13 13:48:12 +1000

    Update app.js

--- Prototype/app.js
+++ Prototype/app.js
@@ -1,11 +1,6 @@
-
-const cp = require('child_process');
 const AppMenus = require('./lib/AppMenus');
 
 const appMenus = new AppMenus();
-
-var menu;
-
 // do some initialisation, make sure docker daemon is open etc
 appMenus.showSub();
-module.exports.menu = menu;
+
