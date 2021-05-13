3787342b41c619e20d3f313ef2643a794fd11dc2
Jamesanthyoung1 <62122658+Jamesanthyoung1@users.noreply.github.com>
2021-05-13 13:45:26 +1000

    Added AppMenus.js and Commands.js
    Moved functions into classes. There is an issue where if you run a method that uses the runCommands method, the title menu breaks when you go back to it.

---
+++ Prototype/lib/AppMenus.js
@@ -0,0 +1,140 @@
+const menuOptions = require('./menuOptions');
+const Commands = require('./Commands');
+const Container = require('./Container');
+const Server = require('./Server');
+const Database = require('./Database');
+const Name = require('./Name');
+
+const commands = new Commands();
+const MenuOptions = new menuOptions();
+const container = new Container();
+const server = new Server();
+const database = new Database();
+const name = new Name();
+
+var menu;
+
+class AppMenus {
+    showMainText() {   // displays the menu for the first 4 main menu options
+        MenuOptions.title();
+        console.log('1. Start Component' + '\n' +
+            '2. Stop Component' + '\n' +
+            '3. Restart Component' + '\n' +
+            '4. New Component' + '\n' +
+            '5. Delete Component' + '\n' +
+            '6. Main Menu' + '\n'
+        );
+    }
+
+    showSub() {
+        MenuOptions.title();
+        console.log('Select component you want to perform actions with:' + '\n' +
+            '1. Container' + '\n' +
+            '2. Server' + '\n' +
+            '3. Database' + '\n' +
+            '4. Name placer' + '\n' +
+            '5. List Docker Images' + '\n' +
+            '6. List Running Containers' + '\n' +
+            '7. Prune Stopped Containers' + '\n' +
+            '8. NA' + '\n' +
+            '9. Build Environment' + '\n' +
+            '0. Exit Application' + '\n'
+        );
+    
+        menu = MenuOptions.refreshMenu(menu);
+        // Ask question
+        menu.question('Enter the number: ', (input) => {
+            this.showMain(input);    // little neater
+        });
+    }
+
+    showMain() {
+        if (arguments[0] == 0) {
+            menu.question('Goodbye! press any key to exit...', (input) => {
+                process.exit(); // show goodbye message then wait for input
+            });
+        }
+        else if (arguments[0] == 1) {
+            this.showMainText();
+            menu = MenuOptions.refreshMenu(menu);
+            menu.question('Please select a number: ', (input) => {
+                switch (input) {
+                    case '1': container.Start(); break;
+                    case '2': container.Stop(); break;
+                    case '3': container.Restart(); break;
+                    case '4': container.New(); break;
+                    case '5': container.Delete(); break;
+                    default: this.showSub();
+                }
+    
+            });
+        }
+        else if (arguments[0] == 2) {
+            this.showMainText();
+            menu = MenuOptions.refreshMenu(menu);
+            menu.question('Please select a number: ', (input) => {
+                switch (input) {
+                    case '1': server.Start(); break;
+                    case '2': server.Stop(); break;
+                    case '3': server.Restart(); break;
+                    case '4': server.New(); break;
+                    case '5': server.Delete(); break;
+                    default: this.showSub();
+                }
+            });
+        }
+        else if (arguments[0] == 3) {
+            this.showMainText();
+            menu = MenuOptions.refreshMenu(menu);
+            menu.question('Please select a number: ', (input) => {
+                switch (input) {
+                    case '1': database.Start(); break;
+                    case '2': database.Stop(); break;
+                    case '3': database.Restart(); break;
+                    case '4': database.New(); break;
+                    case '5': database.Delete(); break;
+                    default: this.showSub();
+                }
+            });
+        }
+        else if (arguments[0] == 4) {
+            this.showMainText();
+            menu = MenuOptions.refreshMenu(menu);
+            menu.question('Please select a number: ', (input) => {
+                switch (input) {
+                    case '1': name.Start(); break;
+                    case '2': name.Stop(); break;
+                    case '3': name.Restart(); break;
+                    case '4': name.New(); break;
+                    case '5': name.Delete(); break;
+                    default: this.showSub();
+                }
+            });
+        }
+        else if (arguments[0] == 5) {   //[Images Returned Successfully] 
+            commands.runCommand('docker images', 'Listing Docker Images', '\nPress Any Key To Continue...', this.showSub, menu);
+        }
+        else if (arguments[0] == 6) {
+            commands.runCommand('docker container ls -a', 'Listing Docker Containers', '\nPress Any Key To Continue...', this.showSub, menu);
+        }
+        else if (arguments[0] == 7) {
+            commands.runCommand('docker container prune -f', 'Remove Stopped Containers', '\nPress Any Key To Continue...', this.showSub, menu);
+            //  runCommand('y', '\nPress Any Key To Continue...', showSub);
+        }
+        else if (arguments[0] == 8) {
+            commands.runCommand('', '', '\nPress Any Key To Continue...', this.showSub, menu);
+        }
+        else if (arguments[0] == 9) {
+            //let com = 'docker-compose -f ' + __dirname + '\\docker-compose_default.yaml up -d'
+    
+            let com = 'docker run -it ubuntu -d';
+            commands.runCommand(com,
+                'Building Environment (this may take some time...)', '\nPress Any Key To Continue...', showSub);
+        }
+        else this.showSub(); // if not a valid argument return to main menu
+    }
+
+}
+
+module.exports.menu = menu;
+module.exports = AppMenus;
---
+++ Prototype/lib/Commands.js
@@ -0,0 +1,46 @@
+const menuOptions = require('./menuOptions');
+const cp = require('child_process');
+const MenuOptions = new menuOptions();
+
+class Commands{
+    //  runs the command in cmd, returns to returnfunction
+    //  titleMessage and returnMessage can be null
+    runCommand(command, titleMessage, returnMessage, returnFunction, menu) {
+        MenuOptions.title();
+        if (titleMessage) console.log(titleMessage);
+    
+        console.log(command);
+        if (returnMessage);
+        else returnMessage = 'Press Any Key to Continue...';
+    
+        menu = MenuOptions.refreshMenu(menu);
+    
+        //  return function is the function to return to after performinig the command
+        //  ie running a command in the main menu should return you to the main menu
+        var commandArray = command.split(' ');
+        var command = commandArray[0];  // first substring is the command
+        commandArray.shift();           // first substring removed, remaining substrings are options
+    
+        console.log('');    // linebreak
+        
+        var ls = cp.spawn(command, commandArray);   // run the command and options in console
+        
+        ls.stdout.on('data', (data) => {      // output return data
+            console.log('' + data);
+        });
+    
+        //  Show errors somehow
+        
+        ls.on('close', (code, signal) => {
+            if (returnFunction) {
+                // use question to wait for input before returning, this is wrapped in the close function so
+                // it will wait for the data to print before continuing
+                menu.question(returnMessage, (input) => {
+                    returnFunction();   // display return message and wait for any input then go to return function }               
+                });
+            }
+        });
+    }
+}
+
+module.exports = Commands;
