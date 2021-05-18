25376cfc14366e4fd72dc63a38947d55ba28af56
Jamesanthyoung1 <62122658+Jamesanthyoung1@users.noreply.github.com>
2021-05-18 11:10:20 +1000

    Update SSH.js
    now able to SSH into multiple vms using the one function

--- Prototype/lib/SSH.js
+++ Prototype/lib/SSH.js
@@ -2,33 +2,35 @@ var Client = require('ssh2').Client;
 var conn = new Client();
 var i;
 
-class SSSH {  
+class SSSH {
   SSH(command, ipAddress, key) {
-    for (i = 0; i < ipAddress.length; i++){
-    console.log(command);
-    console.log(ipAddress[i]);
-    conn.on('ready', () => {
-      console.log('Client :: ready');
-      conn.exec(command, (err, stream) => {
-        if (err) throw err;
-        stream.on('close', (code, signal) => {
-          console.log('Stream :: close :: code: ' + code + ', signal: ' + signal);
-          conn.end();
-        }).on('data', (data) => {
-          console.log('STDOUT: ' + data);
-        }).stderr.on('data', (data) => {
-          console.log('STDERR: ' + data);
+    for (i = 0; i < ipAddress.length; i++) {
+      //console.log(command);
+      //console.log(ipAddress[i]);
+      conn.on('ready', () => {
+        console.log('Client :: ready');
+        conn.exec(command, (err, stream) => {
+          if (err) throw err;
+          stream.on('close', (code, signal) => {
+            console.log('Stream :: close :: code: ' + code + ', signal: ' + signal);
+            conn.end();
+          }).on('data', (data) => {
+            console.log('STDOUT: ' + data);
+          }).stderr.on('data', (data) => {
+            console.log('STDERR: ' + data);
+          });
         });
+      }).connect({
+        host: ipAddress[i],
+        port: 22,
+        username: 'ubuntu',
+        privateKey: require('fs').readFileSync(key)
       });
-    }).connect({
-      host: ipAddress[i],
-      port: 22,
-      username: 'ubuntu',
-      privateKey: require('fs').readFileSync(key)
-    });
+    }
   }
-  }
+}
 
+module.exports = SSSH;
   /*recursiveSSHLoop(command, ipAddress, key) {
     ipArray = ipAddress;
       if (i == (ipArray[i] - 1)) {
@@ -41,11 +43,6 @@ class SSSH {
       }
     }
     */
-}
-
-
-module.exports = SSSH;
-
 // example output:
 // Client :: ready
 // STDOUT: Last login: Sun Jun 15 09:37:21 2014 from 192.168.100.100
