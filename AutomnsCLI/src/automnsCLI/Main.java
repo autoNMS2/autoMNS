389279e82924d6d67397ae4b660a566178501a1b
Jamesanthyoung1 <62122658+Jamesanthyoung1@users.noreply.github.com>
2021-09-15 12:30:30 +1000

    Semester 2 CLI
    CLI for working with agents in app

---
+++ AutomnsCLI/src/automnsCLI/Main.java
@@ -0,0 +1,9 @@
+package automnsCLI;
+
+public class Main {
+
+		public static void main(String[] args) {
+			Menus.MainMenu();		
+		}
+		
+}
---
+++ AutomnsCLI/src/automnsCLI/Menus.java
@@ -0,0 +1,148 @@
+package automnsCLI;
+
+import java.util.Scanner;
+
+public class Menus {
+	
+	public static void MainMenu() {
+		clearScreen();
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Welcome to AutoMNS\n"
+				+ "Please select an option:\n"
+				+ "1. VM Options \n"
+				+ "2. Docker Options \n"
+				+ "3. User Options \n" 
+				+ "4. Open Application \n"
+				+ "0. Exit Application");
+		int i = input.nextInt();
+		
+		switch(i) {
+		case 1:
+			VMOptions();
+			break;
+		case 2:
+			DockerOptions();
+			break;
+		case 3:
+			UserOptions();
+			break;
+		case 4:
+			LaunchApp();
+			break;
+		case 0:
+			System.exit(0);
+		}
+	}
+	
+	public static void VMOptions() {
+		clearScreen();
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Welcome to AutoMNS\n"
+				+ "Please select an option:\n"
+				+ "1. Add VMs \n"
+				+ "2. Initialise VMs \n"
+				+ "3. List VMs \n" 
+				+ "0. Back");
+		int i = input.nextInt();
+		
+		switch(i) {
+		case 1:
+			System.out.println("Adding VMs...");
+			break;
+		case 2:
+			System.out.println("Initilising, please wait...");
+
+			break;
+		case 3:
+			System.out.println("VMs:");
+			break;
+		case 0:
+			MainMenu();
+		}
+	}
+	public static void DockerOptions() {
+		clearScreen();
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Welcome to AutoMNS\n"
+				+ "Please select an option:\n"
+				+ "1. Initialise Swarm \n"
+				+ "2. Add workers to Swarm \n"
+				+ "3. Promote worker to Manager \n" 
+				+ "4. Demote Manager to Worker \n"
+				+ "5. Remove Node \n"
+				+ "6. Inspect Node \n"
+				+ "7. View Node Tasks \n"
+				+ "8. Nuke Swarm \n"
+				+ "0. Back");
+		int i = input.nextInt();
+		
+		switch(i) {
+		case 1:
+			System.out.println("Adding VMs...");
+			break;
+		case 2:
+			System.out.println("Initilising, please wait...");
+			break;
+		case 3:
+			System.out.println("VMs:");
+			break;
+		case 0:
+			MainMenu();
+		}
+	}
+	public static void UserOptions() {
+		clearScreen();
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Welcome to AutoMNS\n"
+				+ "Please select an option:\n"
+				+ "1. Kill Service \n"
+				+ "2. Kill Node \n"
+				+ "3. Nuke Swarm \n"
+				+ "4. List Services \n"
+				+ "5. List Nodes \n"
+				+ "6. Get Service Logs \n"
+				+ "7. Inspect Service \n"
+				+ "0. Back");
+		int i = input.nextInt();
+		
+		switch(i) {
+		case 1:
+			System.out.println("Pick a Service to Kill: ");
+			System.out.println("Killing Service...");
+			break;
+		case 2:
+			System.out.println("Killing Node...");
+			break;
+		case 3:
+			System.out.println("Swarm Nuked");
+			break;
+		case 4:
+			System.out.println("Services: ");
+			break;
+		case 5:
+			System.out.println("Nodes: ");
+			break;
+		case 6:
+			System.out.println("Service Logs: ");
+			break;
+		case 7:
+			System.out.println("Pick a Service to Inspect: ");
+			break;
+		case 0:
+			MainMenu();
+		}
+	}
+	public static void LaunchApp() {
+		clearScreen();
+		System.out.println("Launching App");
+	}
+	
+	public static void clearScreen() {  
+	    System.out.print("\033[H\033[2J");  
+	    System.out.flush();  
+	}
+}
