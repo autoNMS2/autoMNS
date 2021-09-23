23b7b688193d3f84ca91327ef29ffe27d26622b4
sulalabsi <83693029+sulalabsi@users.noreply.github.com>
2021-09-23 23:08:06 +1000

    multi agents

---
+++ AutomnsCLI/src/automnsCLI/multi/recommender_agent.java
@@ -0,0 +1,33 @@
+package automnsCLI;
+
+import jade.core.Agent;
+import jade.core.behaviours.*;
+import jade.lang.acl.*;
+
+public class recommender_agent extends Agent {
+    protected void setup() {
+        // create behaviour for receive and send message to Sender
+        addBehaviour(new CyclicBehaviour(this) {
+            public void action() {
+                //reads and output the received msg
+                ACLMessage msg = receive();
+                if (msg != null) {
+                    String content = msg.getContent();
+                    System.out.println(" Message to " + myAgent.getLocalName()
+                            + " received. Message is : " + content);
+                    switch (content) {
+                        case "Deploy Services":
+                            content = "Recommender service deployed";
+                            break;
+                    }
+                    //sends a reply to the sender
+                    ACLMessage reply = msg.createReply();
+                    reply.setPerformative(ACLMessage.INFORM);
+                    reply.setContent(content);
+                    send(reply);
+                }
+                block();
+            }
+        });
+    }
+}
