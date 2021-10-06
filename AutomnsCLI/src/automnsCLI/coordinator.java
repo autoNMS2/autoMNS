e3e0993fd86d87c7209bd8fe2b8899f5e04b8072
HariPranavoo <pranavoosep2000@gmail.com>
2021-10-06 17:25:10 +1100

    coordinator while loop
    looping to break the error

--- AutomnsCLI/src/automnsCLI/coordinator.java
+++ AutomnsCLI/src/automnsCLI/coordinator.java
@@ -32,24 +32,20 @@ public class coordinator extends Agent
 				"javac -cp autoMNS/jade/lib/jade.jar -d classes autoMNS/AutomnsCLI/src/automnsCLI/multi/webui_agent.java",
 				"java -cp autoMNS/jade/lib/jade.jar:classes jade.Boot -host 172.31.22.170 -agents Webui:automnsCLI.multi.webui_agent"
 				};
+    	String[] ip = {"3.91.243.239","23.22.234.144","34.224.25.217","34.229.134.229","54.91.157.218","54.197.8.110","54.91.228.138" };
+    	int x = 0;
+    	int y =0;
+    	int count =0;
 		//Initializing SSH sessions to each VM, and executing the agent command array
     	try
         {
-			VMFunctions.SSH("3.91.243.239", privateKey, agentCommands[0]);
-			VMFunctions.SSH("3.91.243.239", privateKey, agentCommands[1]);
-			VMFunctions.SSH("23.22.234.144", privateKey, agentCommands[2]);
-			VMFunctions.SSH("23.22.234.144", privateKey, agentCommands[3]);
-			VMFunctions.SSH("34.224.25.217", privateKey, agentCommands[4]);
-			VMFunctions.SSH("34.224.25.217", privateKey, agentCommands[5]);
-			VMFunctions.SSH("34.229.134.229", privateKey, agentCommands[6]);
-			VMFunctions.SSH("34.229.134.229", privateKey, agentCommands[7]);
-			VMFunctions.SSH("54.91.157.218", privateKey, agentCommands[8]);
-			VMFunctions.SSH("54.91.157.218", privateKey, agentCommands[9]);
-			VMFunctions.SSH("54.197.8.110", privateKey, agentCommands[10]);
-			VMFunctions.SSH("54.197.8.110", privateKey, agentCommands[11]);
-			VMFunctions.SSH("54.91.228.138", privateKey, agentCommands[12]);
-			VMFunctions.SSH("54.91.228.138", privateKey, agentCommands[13]);
-		}
+    		do{
+    			
+			VMFunctions.SSH(ip[x], privateKey, agentCommands[y]);
+			VMFunctions.SSH(ip[0+count], privateKey, agentCommands[y]);
+    	x++; y++; count++;
+		}while(x<8);
+        }
 		//Throw a failure in Input & Output operations
         catch (IOException e1)
         {
