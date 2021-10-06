90dc42f28e3be4775121b654ab8da3a0624c52c3
sulalabsi <83693029+sulalabsi@users.noreply.github.com>
2021-10-06 18:05:39 +1100

    test

--- AutomnsCLI/src/automnsCLI/coordinator.java
+++ AutomnsCLI/src/automnsCLI/coordinator.java
@@ -32,19 +32,23 @@ public class coordinator extends Agent
 				"javac -cp autoMNS/jade/lib/jade.jar -d classes autoMNS/AutomnsCLI/src/automnsCLI/multi/webui_agent.java",
 				"java -cp autoMNS/jade/lib/jade.jar:classes jade.Boot -host 172.31.22.170 -agents Webui:automnsCLI.multi.webui_agent"
 				};
-    	String[] ip = {"3.91.243.239","23.22.234.144","34.224.25.217","34.229.134.229","54.91.157.218","54.197.8.110","54.91.228.138" };
-    	int x = 0;
-    	int y =0;
-		//Initializing SSH sessions to each VM, and executing the agent command array
-    	try
-        {
-    		do{
-    			
-			VMFunctions.noOutputSSH(ip[x], privateKey, agentCommands[y]);
-			VMFunctions.noOutputSSH(ip[x], privateKey, agentCommands[y+1]);
-    	x++; y+=2;
-		}while(x<8);
-        }
+		try
+		{
+			VMFunctions.noOutputSSH("3.85.29.60", privateKey, agentCommands[0]);
+			VMFunctions.noOutputSSH("3.85.29.60", privateKey, agentCommands[1]);
+			VMFunctions.noOutputSSH("52.91.100.173", privateKey, agentCommands[2]);
+			VMFunctions.noOutputSSH("52.91.100.173", privateKey, agentCommands[3]);
+			VMFunctions.noOutputSSH("35.175.243.185", privateKey, agentCommands[4]);
+			VMFunctions.noOutputSSH("35.175.243.185", privateKey, agentCommands[5]);
+			VMFunctions.noOutputSSH("54.209.82.120", privateKey, agentCommands[6]);
+			VMFunctions.noOutputSSH("54.209.82.120", privateKey, agentCommands[7]);
+			VMFunctions.noOutputSSH("54.146.135.222", privateKey, agentCommands[8]);
+			VMFunctions.noOutputSSH("54.146.135.222", privateKey, agentCommands[9]);
+			VMFunctions.noOutputSSH("174.129.161.236", privateKey, agentCommands[10]);
+			VMFunctions.noOutputSSH("174.129.161.236", privateKey, agentCommands[11]);
+			VMFunctions.noOutputSSH("54.152.173.110", privateKey, agentCommands[12]);
+			VMFunctions.noOutputSSH("54.152.173.110", privateKey, agentCommands[13]);
+		}
 		//Throw a failure in Input & Output operations
         catch (IOException e1)
         {
