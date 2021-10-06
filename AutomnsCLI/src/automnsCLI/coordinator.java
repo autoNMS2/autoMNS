64704fba2dc3517443c6dd2c19cbcc8ccc2e7554
sulalabsi <83693029+sulalabsi@users.noreply.github.com>
2021-10-06 19:11:58 +1100

    test

--- AutomnsCLI/src/automnsCLI/coordinator.java
+++ AutomnsCLI/src/automnsCLI/coordinator.java
@@ -18,36 +18,34 @@ public class coordinator extends Agent
 		//Compiling, running, and joining main platform command array (for each agent)
     	String[] agentCommands =
                 {"javac -cp autoMNS/jade/lib/jade.jar -d classes autoMNS/AutomnsCLI/src/automnsCLI/multi/db_agent.java",
-				"java -cp autoMNS/jade/lib/jade.jar:classes jade.Boot -host 172.31.27.189 -agents db:automnsCLI.multi.db_agent",
+				"java -cp autoMNS/jade/lib/jade.jar:classes jade.Boot -host 172.31.94.231 -agents db:automnsCLI.multi.db_agent",
 				"javac -cp autoMNS/jade/lib/jade.jar -d classes autoMNS/AutomnsCLI/src/automnsCLI/multi/authenticator_agent.java",
-				"java -cp autoMNS/jade/lib/jade.jar:classes jade.Boot -host 172.31.18.91 -agents Auth:automnsCLI.multi.authenticator_agent",
+				"java -cp autoMNS/jade/lib/jade.jar:classes jade.Boot -host 172.31.85.80 -agents Auth:automnsCLI.multi.authenticator_agent",
 				"javac -cp autoMNS/jade/lib/jade.jar -d classes autoMNS/AutomnsCLI/src/automnsCLI/multi/image_agent.java",
-				"java -cp autoMNS/jade/lib/jade.jar:classes jade.Boot -host 172.31.27.77 -agents Image:automnsCLI.multi.image_agent",
+				"java -cp autoMNS/jade/lib/jade.jar:classes jade.Boot -host 172.31.83.130 -agents Image:automnsCLI.multi.image_agent",
 				"javac -cp autoMNS/jade/lib/jade.jar -d classes autoMNS/AutomnsCLI/src/automnsCLI/multi/persistence_agent.java",
-				"java -cp autoMNS/jade/lib/jade.jar:classes jade.Boot -host 172.31.25.182 -agents Persistence:automnsCLI.multi.persistence_agent",
+				"java -cp autoMNS/jade/lib/jade.jar:classes jade.Boot -host 172.31.83.157 -agents Persistence:automnsCLI.multi.persistence_agent",
 				"javac -cp autoMNS/jade/lib/jade.jar -d classes autoMNS/AutomnsCLI/src/automnsCLI/multi/recommender_agent.java",
-				"java -cp autoMNS/jade/lib/jade.jar:classes jade.Boot -host 172.31.28.168 -agents Recommender:automnsCLI.multi.recommender_agent",
+				"java -cp autoMNS/jade/lib/jade.jar:classes jade.Boot -host 172.31.82.207 -agents Recommender:automnsCLI.multi.recommender_agent",
 				"javac -cp autoMNS/jade/lib/jade.jar -d classes autoMNS/AutomnsCLI/src/automnsCLI/multi/registry_agent.java",
-				"java -cp autoMNS/jade/lib/jade.jar:classes jade.Boot -host 172.31.19.8 -agents Registry:automnsCLI.multi.registry_agent",
+				"java -cp autoMNS/jade/lib/jade.jar:classes jade.Boot -host 172.31.85.39 -agents Registry:automnsCLI.multi.registry_agent",
 				"javac -cp autoMNS/jade/lib/jade.jar -d classes autoMNS/AutomnsCLI/src/automnsCLI/multi/webui_agent.java",
-				"java -cp autoMNS/jade/lib/jade.jar:classes jade.Boot -host 172.31.22.170 -agents Webui:automnsCLI.multi.webui_agent"
+				"java -cp autoMNS/jade/lib/jade.jar:classes jade.Boot -host 172.31.95.28 -agents Webui:automnsCLI.multi.webui_agent"
 				};
 
-    	String[] ip = {"3.91.243.239","23.22.234.144","34.224.25.217","34.229.134.229","54.91.157.218","54.197.8.110","54.91.228.138" };
+    	String[] ip = {"18.212.74.131","35.171.161.216","35.174.111.173","3.83.106.197","18.233.166.97","3.83.193.82","3.87.208.125" };
     	int x = 0;
     	int y = 0;
-	int z = 1;
-		//Initializing SSH sessions to each VM, and executing the agent command array
+		int z = 1;
     	try
         {
-    		do{
-    			
-			VMFunctions.noOutputSSH(ip[x], privateKey, agentCommands[y]);
-			VMFunctions.noOutputSSH(ip[x], privateKey, agentCommands[y+1]);
-    	x++; y+=2; z++;
-		}while(z<8);
+    		do
+			{
+				VMFunctions.noOutputSSH(ip[x], privateKey, agentCommands[y]);
+				VMFunctions.noOutputSSH(ip[x], privateKey, agentCommands[y+1]);
+				x++; y+=2; z++;
+			} while(z<8);
         }
-
 		//Throw a failure in Input & Output operations
         catch (IOException e1)
         {
