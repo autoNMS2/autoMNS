e6541578a1909f3251dc4952cf561de4ecef805f
sulalabsi <83693029+sulalabsi@users.noreply.github.com>
2021-10-06 20:23:47 +1100

    test

--- jade/src/w9test/send.java
+++ jade/src/w9test/send.java
@@ -21,9 +21,9 @@ public class send extends Agent {
         // creates a msg and send to a specified agent (a1)
         ACLMessage msg = new ACLMessage(ACLMessage.INFORM);
         //uses GUID with a1 name,platform name, and default port
-        AID dest = new AID("a1@172.31.19.134:1099/JADE", AID.ISGUID);
+        AID dest = new AID("a1@172.31.30.182:1099/JADE", AID.ISGUID);
         //uses MTP address of a1 platform and default port
-        dest.addAddresses("http://172.31.19.134:7778/acc");
+        dest.addAddresses("http://172.31.30.182:7778/acc");
         msg.addReceiver(dest);
         msg.setContent("Hi from Sender!");
         send(msg);
