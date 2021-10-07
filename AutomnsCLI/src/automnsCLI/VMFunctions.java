192fb8193064451f68e27183fd781b89a9c57afa
Jamesanthyoung1 <62122658+Jamesanthyoung1@users.noreply.github.com>
2021-10-07 14:20:51 +1100

    Update VMFunctions.java
    Program now takes a VMConfig file consisting of the local ssh key file path, the repo ssh key file path, then each of the 8 vm's private ip followed by it's public ip

--- AutomnsCLI/src/automnsCLI/VMFunctions.java
+++ AutomnsCLI/src/automnsCLI/VMFunctions.java
@@ -17,14 +17,43 @@ import com.jcraft.jsch.*;
 
 public class VMFunctions {
 	
-	private static List<String> VMs;
+	private static String localKeyFilePath;
 	
-	public static List<String> getVMS(){
-		return VMs;
+	public static String getLocalKeyFilePath() {
+		return localKeyFilePath;
 	}
 	
-	public static void setVMS(List<String> newVMS) {
-		VMFunctions.VMs = newVMS;
+	public static void setLocalKeyFilePath(String localPath) {
+		VMFunctions.localKeyFilePath = localPath;
+	}
+	
+	private static String repoKeyFilePath;
+	
+	public static String getRepoKeyFilePath() {
+		return repoKeyFilePath;
+	}
+	
+	public static void setRepoKeyFilePath(String repoPath) {
+		VMFunctions.repoKeyFilePath = repoPath;
+	}
+	
+	private static List<String> VMPublicIps;
+	
+	public static List<String> getVMPublicIps(){
+		return VMPublicIps;
+	}
+	
+	public static void setVMPublicIps(List<String> newPublicVMS) {
+		VMFunctions.VMPublicIps = newPublicVMS;
+	}
+	private static List<String> VMPrivateIps;
+	
+	public static List<String> getVMPrivateIps(){
+		return VMPrivateIps;
+	}
+	
+	public static void setVMPrivateIps(List<String> newPrivateVMS) {
+		VMFunctions.VMPrivateIps = newPrivateVMS;
 	}
 	
 	//this array of commands is used to run on the vms to bring them up to a working state 
@@ -44,70 +73,77 @@ public class VMFunctions {
 	//to the working level we need them at to run the program
 	public static void addVMs() throws IOException {
 		//create a list variable and read the user's ips from the file
-		List<String> vms = new ArrayList<>();
-		vms = getVmConfig();
-		//set the vms list
-		setVMS(vms);
-		//update vms
-		updateVMs();
-		// add the first vm to a swarm and get the swarm token
-		addVMsToSwarm();
-		// install git and pull repository on vms
-		gitSetup();
-		// install java on vms
-		javaSetup();
-		//wait for user input before returning to main menu
-        System.out.println("VMs initialised.\n" + "Press any key and then enter to return to main menu...");
-		String userInput;
-		Scanner input = new Scanner(System.in);
-		userInput = input.next();
-		Menus.MainMenu();
+			if (getVmConfig()) {
+			//update vms
+			updateVMs();
+			// add the first vm to a swarm and get the swarm token
+			addVMsToSwarm();
+			// install git and pull repository on vms
+			gitSetup();
+			// install java on vms
+			javaSetup();
+			//wait for user input before returning to main menu
+	        System.out.println("VMs initialised.\n" + "Press any key and then enter to return to main menu...");
+			String userInput;
+			Scanner input = new Scanner(System.in);
+			userInput = input.next();
+			Menus.MainMenu();
+			}
+			else {
+				System.out.println("VM Config file incorrectly formatted");
+			}
 		}
 	
 	public static void updateVMs() throws IOException {
 		//runs commands on each of the user's vms that updates them
-		List<String> localVMs = getVMS();
-		String privateKey = localVMs.get(0);
-		for (int j = 1; j < localVMs.size(); j++) {
+		List<String> localVMs = getVMPublicIps();
+		String privateKey = getLocalKeyFilePath();
+		for (int j = 0; j < localVMs.size(); j++) {
 			SSH(localVMs.get(j), privateKey, vmCommands[0]);
 		}
 	}
 	
 	public static void addVMsToSwarm() throws IOException {
 		//makes the first vm in the list a swarm manager, then adds all other vms to swarm as workers
-		List<String> localVMs = getVMS();
-		String privateKey = localVMs.get(0);
+		List<String> localVMs = getVMPublicIps();
+		String privateKey = getLocalKeyFilePath();
 		// add the first vm to a swarm and get the swarm token
-		String output = SSH(localVMs.get(1), privateKey, vmCommands[1] + localVMs.get(1));
+		String output = SSH(localVMs.get(0), privateKey, vmCommands[1] + localVMs.get(0));
 		String joinToken = "sudo " + output.substring(142, 273);
 		// add other vms to swarm
-		for (int j = 2; j < localVMs.size(); j++) {
+		for (int j = 1; j < localVMs.size(); j++) {
 			SSH(localVMs.get(j), privateKey, joinToken);
 		}
 	}
 	
 	public static void gitSetup() throws IOException {
 		//installs git on vms then pulls autoMNS repository to vms
-		List<String> localVMs = getVMS();
-		String privateKey = localVMs.get(0);
-		for (int j = 1; j < localVMs.size(); j++) {
+		List<String> localVMs = getVMPublicIps();
+		String privateKey = getLocalKeyFilePath();
+		for (int j = 0; j < localVMs.size(); j++) {
 			SSH(localVMs.get(j), privateKey, vmCommands[2]);
 		}
 	}
 	
 	public static void javaSetup() throws IOException {
 		//installs jdk and jre on each vm
-		List<String> localVMs = getVMS();
-		String privateKey = localVMs.get(0);
-		for (int j = 1; j < localVMs.size(); j++) {
+		List<String> localVMs = getVMPublicIps();
+		String privateKey = getLocalKeyFilePath();
+		for (int j = 0; j < localVMs.size(); j++) {
 			SSH(localVMs.get(j), privateKey, vmCommands[4]);
 		}
 	}
 	
 	//this function reads in the user's vm configuration file and saves the ip addresses 
 	//to a list of strings and returns it
-	public static List<String> getVmConfig(){
-		List<String> localVmsList = new ArrayList<>();
+	public static boolean getVmConfig(){
+		String localKeyPath;
+		String repoKeyPath;
+		
+		List<String> ipConfigList = new ArrayList<>();
+		List<String> localPrivateVmsList = new ArrayList<>();
+		List<String> localPublicVmsList = new ArrayList<>();
+
 		Scanner input = new Scanner(System.in);
 		System.out.println("Welcome to AutoMNS\n" + "Please enter the path of your VM config file: ");
 
@@ -116,52 +152,64 @@ public class VMFunctions {
 		      File myObj = new File(filePath);
 		      Scanner myReader = new Scanner(myObj);
 		      while (myReader.hasNextLine()) {
-		    	  localVmsList.add(myReader.nextLine().replaceAll("\\s+",""));
+		    	  ipConfigList.add(myReader.nextLine().replaceAll("\\s+",""));
 		      }
 		      myReader.close();
 		    } catch (FileNotFoundException e) {
 		      System.out.println("An error occurred.");
 		      e.printStackTrace();
 		    }
-		return localVmsList;	
+		localKeyPath = ipConfigList.get(0);
+		repoKeyPath = ipConfigList.get(1);
+		for (int i = 2; i < ipConfigList.size(); i+=2) {
+			localPrivateVmsList.add(ipConfigList.get(i));
+			localPublicVmsList.add(ipConfigList.get(i+1));
+		}
+		setLocalKeyFilePath(localKeyPath);
+		setRepoKeyFilePath(repoKeyPath);
+		setVMPrivateIps(localPrivateVmsList);
+		setVMPublicIps(localPublicVmsList);
+		return true;	
 	}
 	
 
 	public static void initialiseAgents() throws IOException {		
 		//get the list of vms from the user, if the config file has not yet been input then the length will be 0 and the user will be asked to input
-		List<String> vms = getVMS();
-			if (vms.size() == 0) {
-				vms = getVmConfig();
+		List<String> publicVms = getVMPublicIps();
+		if (publicVms == null) {
+			getVmConfig();
+			publicVms = getVMPublicIps();
 		}
+		List<String> privateVms = getVMPrivateIps();
+		String repoPrivateKey = getRepoKeyFilePath();
+
 		//The following array contains the commands necessary to initialise the coordinator agent on the first vm provided by the user
 		String[] agentCommands = 
 			{ "javac -cp autoMNS/jade/lib/jade.jar:autoMNS/jade/lib/jsch-0.1.55.jar -d classes autoMNS/AutomnsCLI/src/automnsCLI/*.java",
-			  "java -cp autoMNS/jade/lib/jade.jar:autoMNS/jade/lib/jsch-0.1.55.jar:classes jade.Boot -agents coordinator:automnsCLI.JAMEScoordinator ("};	
-		//the following for loop adds the users vm ips as arguments on to the end of the java command
+			  "java -cp autoMNS/jade/lib/jade.jar:autoMNS/jade/lib/jsch-0.1.55.jar:classes jade.Boot -agents 'coordinator:automnsCLI.coordinator ("};	
+		//the following for loops adds the users public and private vm ips as arguments on to the end of the java command
 		//this allows the coordinator agent to access these ip addresses
-		for (int i = 0; i < vms.size(); i++) {
-			if (i != vms.size() - 1) {
-			agentCommands[1] += vms.get(i) + ",";
-			}
-			else {
-				agentCommands[1] += vms.get(i);
-			}
+		for (int i = 0; i < privateVms.size(); i++) {
+			agentCommands[1] += privateVms.get(i) + ",";
+		}	
+		for (int i = 0; i < publicVms.size(); i++) {
+			agentCommands[1] += publicVms.get(i) + ",";
 		}	
 		//add a closing bracket to the end of the java command
-		agentCommands[1] += ")";
+		agentCommands[1] += repoPrivateKey + ")'";
 
-		//first element in vms should always be private key path
-		String privateKey = vms.get(0);
+		//get the local ssh key file path to ssh into coordinator
+		String localPrivateKey = getLocalKeyFilePath();
 		// intialise the platform on the coordinator agent
 		// coordinator should deploy other agents from it's own code
-		SSH(vms.get(1), privateKey, agentCommands[0]);
-		SSH(vms.get(1), privateKey, agentCommands[1]);
+		SSH(publicVms.get(0), localPrivateKey, agentCommands[0]);
+		SSH(publicVms.get(0), localPrivateKey, agentCommands[1]);
 		System.out.println("Agents Deployed");
 		Menus.MainMenu();
 	}
 	
 	public static void launchApplication() throws IOException {
-		List<String> vms = getVMS();
+		List<String> vms = getVMPublicIps();
 		System.out.println("Launching App");
 		String URL = "https://" + vms.get(1) + "/8080";
 		openWebpage(URL);
