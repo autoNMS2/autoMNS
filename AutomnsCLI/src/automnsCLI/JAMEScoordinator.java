7c0f660c27d54b4b029e40d59083d50f6c11d761
Jamesanthyoung1 <62122658+Jamesanthyoung1@users.noreply.github.com>
2021-10-08 13:44:14 +1100

    Update JAMEScoordinator.java

--- AutomnsCLI/src/automnsCLI/JAMEScoordinator.java
+++ AutomnsCLI/src/automnsCLI/JAMEScoordinator.java
@@ -126,34 +126,61 @@ public class JAMEScoordinator extends Agent
 
         		ACLMessage msg = new ACLMessage(ACLMessage.INFORM);
         		//
-        		AID dest = new AID("db@172.31.81.121:5001/JADE", AID.ISGUID);
-        		dest.addAddresses("http://172.31.81.121:7778/acc");
+//        		AID dest = new AID("db@172.31.81.121:5001/JADE", AID.ISGUID);
+//        		dest.addAddresses("http://172.31.81.121:7778/acc");
+//        		msg.addReceiver(dest);
+//        		//
+//        		AID dest1 = new AID("auth@172.31.90.166:5002/JADE", AID.ISGUID);
+//        		dest1.addAddresses("http://172.31.90.166:7778/acc");
+//        		msg.addReceiver(dest1);
+//        		//
+//        		AID dest2 = new AID("image@172.31.82.29:5003/JADE", AID.ISGUID);
+//        		dest2.addAddresses("http://172.31.82.29:7778/acc");
+//        		msg.addReceiver(dest2);
+//        		//
+//        		AID dest3 = new AID("pers@172.31.89.127:5004/JADE", AID.ISGUID);
+//        		dest3.addAddresses("http://172.31.89.127:7778/acc");
+//        		msg.addReceiver(dest3);
+//        		//
+//        		AID dest4 = new AID("recom@172.31.89.44:5005/JADE", AID.ISGUID);
+//        		dest4.addAddresses("http://172.31.89.44:7778/acc");
+//        		msg.addReceiver(dest4);
+//        		//
+//        		AID dest5 = new AID("reg@172.31.91.28:5006/JADE", AID.ISGUID);
+//        		dest5.addAddresses("http://172.31.91.28:7778/acc");
+//        		msg.addReceiver(dest5);
+//        		//
+//        		AID dest6 = new AID("webui@172.31.94.95:5007/JADE", AID.ISGUID);
+//        		dest6.addAddresses("http://172.31.94.95:7778/acc");
+//        		msg.addReceiver(dest6);
+        		//
+        		AID dest = new AID("db@172.31.83.213:5001/JADE", AID.ISGUID);
+        		dest.addAddresses("http://172.31.83.213:7778/acc");
         		msg.addReceiver(dest);
         		//
-        		AID dest1 = new AID("auth@172.31.90.166:5002/JADE", AID.ISGUID);
-        		dest1.addAddresses("http://172.31.90.166:7778/acc");
+        		AID dest1 = new AID("auth@172.31.94.8:5002/JADE", AID.ISGUID);
+        		dest1.addAddresses("http://172.31.94.8:7778/acc");
         		msg.addReceiver(dest1);
         		//
-        		AID dest2 = new AID("image@172.31.82.29:5003/JADE", AID.ISGUID);
-        		dest2.addAddresses("http://172.31.82.29:7778/acc");
+        		AID dest2 = new AID("image@172.31.82.12:5003/JADE", AID.ISGUID);
+        		dest2.addAddresses("http://172.31.82.12:7778/acc");
         		msg.addReceiver(dest2);
         		//
-        		AID dest3 = new AID("pers@172.31.89.127:5004/JADE", AID.ISGUID);
-        		dest3.addAddresses("http://172.31.89.127:7778/acc");
+        		AID dest3 = new AID("pers@172.31.89.126:5004/JADE", AID.ISGUID);
+        		dest3.addAddresses("http://172.31.89.126:7778/acc");
         		msg.addReceiver(dest3);
         		//
-        		AID dest4 = new AID("recom@172.31.89.44:5005/JADE", AID.ISGUID);
-        		dest4.addAddresses("http://172.31.89.44:7778/acc");
+        		AID dest4 = new AID("recom@172.31.82.46:5005/JADE", AID.ISGUID);
+        		dest4.addAddresses("http://172.31.82.46:7778/acc");
         		msg.addReceiver(dest4);
         		//
-        		AID dest5 = new AID("reg@172.31.91.28:5006/JADE", AID.ISGUID);
-        		dest5.addAddresses("http://172.31.91.28:7778/acc");
+        		AID dest5 = new AID("reg@172.31.80.110:5006/JADE", AID.ISGUID);
+        		dest5.addAddresses("http://172.31.80.110:7778/acc");
         		msg.addReceiver(dest5);
         		//
-        		AID dest6 = new AID("webui@172.31.94.95:5007/JADE", AID.ISGUID);
-        		dest6.addAddresses("http://172.31.94.95:7778/acc");
+        		AID dest6 = new AID("webui@172.31.24.6:5007/JADE", AID.ISGUID);
+        		dest6.addAddresses("http://172.31.24.6:7778/acc");
         		msg.addReceiver(dest6);
-        		//
         		msg.setContent(msgContent);
         		send(msg);
                 break;
