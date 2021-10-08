fcd23f1564beac0ecfbc327d9c261b2f3466f0db
Jamesanthyoung1 <62122658+Jamesanthyoung1@users.noreply.github.com>
2021-10-08 12:44:23 +1100

    Update JAMEScoordinator.java
    added message functionality

--- AutomnsCLI/src/automnsCLI/JAMEScoordinator.java
+++ AutomnsCLI/src/automnsCLI/JAMEScoordinator.java
@@ -9,26 +9,40 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
 
 import automnsCLI.VMFunctions;
 
 public class JAMEScoordinator extends Agent
 {
-    protected void setup(){
-        addBehaviour(new CyclicBehaviour(this) {
-            public void action() {
-                ACLMessage msg = receive();
-                if (msg != null) {
-                    System.out.println("Message" + msg.getContent()
-                            + " ( " + msg.getSender().getName() + " )");
-                }
-                try {
-                    menu();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        });
+	public boolean checkmsg() {
+
+		ACLMessage msg = receive();
+		if (msg != null) {
+
+			System.out.println("Message" + msg.getContent()
+					+ " ( " + msg.getSender().getName() + " )");
+
+			return true;
+		}
+		return false;
+	}
+	protected void setup() {
+		addBehaviour(new CyclicBehaviour(this) {
+
+			public void action() {
+				try {
+					TimeUnit.SECONDS.sleep(1);
+				}catch (InterruptedException e) {
+				}
+				while (checkmsg());
+				try {
+					menu();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		});
     }
     public void menu () throws IOException {
         //Defining the path to the AWS key
@@ -48,20 +62,20 @@ public class JAMEScoordinator extends Agent
     	
 		//Compiling, running, and joining main platform command array (for each agent)
     	String[] agentCommands =
-                {"javac -cp autoMNS/jade/lib/jade.jar -d classes autoMNS/AutomnsCLI/src/automnsCLI/multi/db_agent.java",
-				"java -cp autoMNS/jade/lib/jade.jar:classes jade.Boot -host " + workerVMsPrivate.get(0) + " -agents db:automnsCLI.multi.db_agent",
-				"javac -cp autoMNS/jade/lib/jade.jar -d classes autoMNS/AutomnsCLI/src/automnsCLI/multi/authenticator_agent.java",
-				"java -cp autoMNS/jade/lib/jade.jar:classes jade.Boot -host " + workerVMsPrivate.get(1) + " -agents Auth:automnsCLI.multi.authenticator_agent",
-				"javac -cp autoMNS/jade/lib/jade.jar -d classes autoMNS/AutomnsCLI/src/automnsCLI/multi/image_agent.java",
-				"java -cp autoMNS/jade/lib/jade.jar:classes jade.Boot -host " + workerVMsPrivate.get(2) + " -agents Image:automnsCLI.multi.image_agent",
-				"javac -cp autoMNS/jade/lib/jade.jar -d classes autoMNS/AutomnsCLI/src/automnsCLI/multi/persistence_agent.java",
-				"java -cp autoMNS/jade/lib/jade.jar:classes jade.Boot -host " + workerVMsPrivate.get(3) + " -agents Persistence:automnsCLI.multi.persistence_agent",
-				"javac -cp autoMNS/jade/lib/jade.jar -d classes autoMNS/AutomnsCLI/src/automnsCLI/multi/recommender_agent.java",
-				"java -cp autoMNS/jade/lib/jade.jar:classes jade.Boot -host " + workerVMsPrivate.get(4) + " -agents Recommender:automnsCLI.multi.recommender_agent",
-				"javac -cp autoMNS/jade/lib/jade.jar -d classes autoMNS/AutomnsCLI/src/automnsCLI/multi/registry_agent.java",
-				"java -cp autoMNS/jade/lib/jade.jar:classes jade.Boot -host " + workerVMsPrivate.get(5) + " -agents Registry:automnsCLI.multi.registry_agent",
-				"javac -cp autoMNS/jade/lib/jade.jar -d classes autoMNS/AutomnsCLI/src/automnsCLI/multi/webui_agent.java",
-				"java -cp autoMNS/jade/lib/jade.jar:classes jade.Boot -host " + workerVMsPrivate.get(6) + " -agents Webui:automnsCLI.multi.webui_agent"
+                {"javac -cp autoMNS/jade/lib/jade.jar -d classes autoMNS/AutomnsCLI/src/automnsCLI/multi/Worker.java",
+				"java -cp autoMNS/jade/lib/jade.jar:classes jade.Boot -host " + workerVMsPrivate.get(0) + " -port 5001 -agents db:automnsCLI.multi.Worker",
+				"javac -cp autoMNS/jade/lib/jade.jar -d classes autoMNS/AutomnsCLI/src/automnsCLI/multi/Worker.java",
+				"java -cp autoMNS/jade/lib/jade.jar:classes jade.Boot -host " + workerVMsPrivate.get(1) + " -port 5002  -agents Auth:automnsCLI.multi.Worker",
+				"javac -cp autoMNS/jade/lib/jade.jar -d classes autoMNS/AutomnsCLI/src/automnsCLI/multi/Worker.java",
+				"java -cp autoMNS/jade/lib/jade.jar:classes jade.Boot -host " + workerVMsPrivate.get(2) + " -port 5003  -agents Image:automnsCLI.multi.Worker",
+				"javac -cp autoMNS/jade/lib/jade.jar -d classes autoMNS/AutomnsCLI/src/automnsCLI/multi/Worker.java",
+				"java -cp autoMNS/jade/lib/jade.jar:classes jade.Boot -host " + workerVMsPrivate.get(3) + " -port 5004  -agents Persistence:automnsCLI.multi.Worker",
+				"javac -cp autoMNS/jade/lib/jade.jar -d classes autoMNS/AutomnsCLI/src/automnsCLI/multi/Worker.java",
+				"java -cp autoMNS/jade/lib/jade.jar:classes jade.Boot -host " + workerVMsPrivate.get(4) + " -port 5005  -agents Recommender:automnsCLI.multi.Worker",
+				"javac -cp autoMNS/jade/lib/jade.jar -d classes autoMNS/AutomnsCLI/src/automnsCLI/multi/Worker.java",
+				"java -cp autoMNS/jade/lib/jade.jar:classes jade.Boot -host " + workerVMsPrivate.get(5) + " -port 5006  -agents Registry:automnsCLI.multi.Worker",
+				"javac -cp autoMNS/jade/lib/jade.jar -d classes autoMNS/AutomnsCLI/src/automnsCLI/multi/Worker.java",
+				"java -cp autoMNS/jade/lib/jade.jar:classes jade.Boot -host " + workerVMsPrivate.get(6) + " -port 5007  -agents Webui:automnsCLI.multi.Worker"
 				};
 
         System.out.println("select command: " +
@@ -109,16 +123,39 @@ public class JAMEScoordinator extends Agent
                 }
                 break;
             case 3:
-                msgContent = "Get Services Update";
-                ACLMessage msg = new ACLMessage(ACLMessage.INFORM);
-                AID dest = new AID("db@172.31.88.236:1009/JADE", AID.ISGUID);
-                //AID dest1 = new AID("auth@172.31.85.143:5001/JADE", AID.ISGUID);
-                dest.addAddresses("http://172.31.88.236:7778/acc");
-                //dest1.addAddresses("http://172.31.85.143:7778/acc");
-                msg.addReceiver(dest);
-               // msg.addReceiver(dest1);
-                msg.setContent(msgContent);
-                send(msg);
+
+        		ACLMessage msg = new ACLMessage(ACLMessage.INFORM);
+        		//
+        		AID dest = new AID("db@" + workerVMsPrivate.get(0) +":5001/JADE", AID.ISGUID);
+        		dest.addAddresses("http://" + workerVMsPrivate.get(0) +":7778/acc");
+        		msg.addReceiver(dest);
+        		//
+        		AID dest1 = new AID("auth@" + workerVMsPrivate.get(1) +":5002/JADE", AID.ISGUID);
+        		dest1.addAddresses("http://" + workerVMsPrivate.get(1) +":7778/acc");
+        		msg.addReceiver(dest1);
+        		//
+        		AID dest2 = new AID("image@" + workerVMsPrivate.get(2) +":5003/JADE", AID.ISGUID);
+        		dest2.addAddresses("http://" + workerVMsPrivate.get(2) +":7778/acc");
+        		msg.addReceiver(dest2);
+        		//
+        		AID dest3 = new AID("pers@" + workerVMsPrivate.get(3) +":5004/JADE", AID.ISGUID);
+        		dest3.addAddresses("http://" + workerVMsPrivate.get(3) +":7778/acc");
+        		msg.addReceiver(dest3);
+        		//
+        		AID dest4 = new AID("recom@" + workerVMsPrivate.get(4) +":5005/JADE", AID.ISGUID);
+        		dest4.addAddresses("http://" + workerVMsPrivate.get(4) +":7778/acc");
+        		msg.addReceiver(dest4);
+        		//
+        		AID dest5 = new AID("reg@" + workerVMsPrivate.get(5) +":5006/JADE", AID.ISGUID);
+        		dest5.addAddresses("http://" + workerVMsPrivate.get(5) +":7778/acc");
+        		msg.addReceiver(dest5);
+        		//
+        		AID dest6 = new AID("webui@" + workerVMsPrivate.get(6) +":5007/JADE", AID.ISGUID);
+        		dest6.addAddresses("http://" + workerVMsPrivate.get(6) +":7778/acc");
+        		msg.addReceiver(dest6);
+        		//
+        		msg.setContent(msgContent);
+        		send(msg);
                 break;
         }
     } 
