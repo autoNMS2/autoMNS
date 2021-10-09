3b0c93702062a41c6be9545fa4815aeb91a7dd71
sulalabsi <83693029+sulalabsi@users.noreply.github.com>
2021-10-09 13:25:24 +1100

    fix/test

--- AutomnsCLI/src/automnsCLI/sendcoord.java
+++ AutomnsCLI/src/automnsCLI/sendcoord.java
@@ -87,7 +87,7 @@ public class sendcoord extends Agent {
 						VMFunctions.noOutputSSH(ip[x], privateKey, agentCommands[y]);
 						VMFunctions.noOutputSSH(ip[x], privateKey, agentCommands[y+1]);
 						x++; y+=2; z++;
-					} while(y<7);
+					} while(y<9);
 				}
 				//Throw a failure in Input & Output operations
 				catch (IOException e1)
