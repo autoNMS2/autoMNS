04b26029949da03760bbe0a43a3687626ffc3f4a
sulalabsi <83693029+sulalabsi@users.noreply.github.com>
2021-10-09 13:37:13 +1100

    fix/test

--- AutomnsCLI/src/automnsCLI/sendcoord.java
+++ AutomnsCLI/src/automnsCLI/sendcoord.java
@@ -32,6 +32,8 @@ public class sendcoord extends Agent {
 		String privateKey = "autoMNS/jade/src/test0/test.pem";
 		String[] agentCommands =
 				{"javac -cp autoMNS/jade/lib/jade.jar -d classes autoMNS/AutomnsCLI/src/automnsCLI/multi/receive0.java",
+						"java -cp autoMNS/jade/lib/jade.jar:classes jade.Boot -host 172.31.28.122 -port 1099 -local-host 172.31.16.27 -local-port 5000 -container Database:automnsCLI.receive0",
+						"javac -cp autoMNS/jade/lib/jade.jar -d classes autoMNS/AutomnsCLI/src/automnsCLI/multi/receive0.java",
 						"java -cp autoMNS/jade/lib/jade.jar:classes jade.Boot -host 172.31.28.122 -port 1099 -local-host 172.31.21.119 -local-port 5001 -container Authenticator:automnsCLI.receive0",
 						"javac -cp autoMNS/jade/lib/jade.jar -d classes autoMNS/AutomnsCLI/src/automnsCLI/multi/receive0.java",
 						"java -cp autoMNS/jade/lib/jade.jar:classes jade.Boot -host 172.31.28.122 -port 1099 -local-host 172.31.21.120 -local-port 5002 -container Image:automnsCLI.receive0",
@@ -69,7 +71,7 @@ public class sendcoord extends Agent {
 				msgContent = "Deploy Agents";
 				ACLMessage msg = new ACLMessage(ACLMessage.INFORM);
 				msg.setContent(msgContent);
-//				msg.addReceiver(new AID("Database", AID.ISLOCALNAME));
+				msg.addReceiver(new AID("Database", AID.ISLOCALNAME));
 				msg.addReceiver(new AID("Authenticator", AID.ISLOCALNAME));
 				msg.addReceiver(new AID("Image", AID.ISLOCALNAME));
 				msg.addReceiver(new AID("Persistence", AID.ISLOCALNAME));
@@ -80,7 +82,7 @@ public class sendcoord extends Agent {
 				break;
 			case 2:
 				//String[] ip = {"3.80.113.155"};
-				String[] ip = {"54.147.29.37","54.235.236.254","18.212.96.141","34.207.80.67","34.224.68.95","34.230.29.173"};
+				String[] ip = {"3.80.113.155","54.147.29.37","54.235.236.254","18.212.96.141","34.207.80.67","34.224.68.95","34.230.29.173"};
 				//String[] ip = {"3.80.113.155","54.147.29.37","54.235.236.254","18.212.96.141","34.207.80.67","34.224.68.95","34.230.29.173"};
 				int x = 0;
 				int y = 0;
@@ -92,7 +94,7 @@ public class sendcoord extends Agent {
 						VMFunctions.noOutputSSH(ip[x], privateKey, agentCommands[y]);
 						VMFunctions.noOutputSSH(ip[x], privateKey, agentCommands[y+1]);
 						x++; y+=2; z++;
-					} while(z<6);
+					} while(z<7);
 				}
 				//Throw a failure in Input & Output operations
 				catch (IOException e1)
