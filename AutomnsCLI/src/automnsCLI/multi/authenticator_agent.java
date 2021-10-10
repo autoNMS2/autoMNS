6d8024dc1c91d8aae703ce3d6883efee315fd587
sulalabsi <83693029+sulalabsi@users.noreply.github.com>
2021-10-10 20:53:15 +1100

    test

--- AutomnsCLI/src/automnsCLI/multi/authenticator_agent.java
+++ AutomnsCLI/src/automnsCLI/multi/authenticator_agent.java
@@ -46,6 +46,7 @@ public class authenticator_agent extends Agent {
                                     //close the file
                                     log.close();
                                 }
+                                System.out.println(s);
 
                                 // Read any errors from the attempted command
                                 System.out.println("Here is the standard error of the command (if any):\n");
