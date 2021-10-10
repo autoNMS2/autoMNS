89de77505290a6b1adab5001d3e3440b387ed127
sulalabsi <83693029+sulalabsi@users.noreply.github.com>
2021-10-10 20:47:07 +1100

    test

--- AutomnsCLI/src/automnsCLI/multi/authenticator_agent.java
+++ AutomnsCLI/src/automnsCLI/multi/authenticator_agent.java
@@ -38,9 +38,10 @@ public class authenticator_agent extends Agent {
                                 // Read the output from the command
                                 System.out.println("Here is the standard output of the command:\n");
                                 String s = null;
+                                FileWriter log=new FileWriter("autoMNS/log.txt");
                                 while ((s = stdInput.readLine()) != null) {
                                     System.out.println(s);
-                                    FileWriter log=new FileWriter("autoMNS/log.txt");
+
                                     log.write(s);
                                     System.out.println("Writing successful");
                                     //close the file
@@ -49,7 +50,6 @@ public class authenticator_agent extends Agent {
 
                                 // Read any errors from the attempted command
                                 System.out.println("Here is the standard error of the command (if any):\n");
-                                System.out.println(log);
                                 while ((s = stdError.readLine()) != null) {
                                     System.out.println(s);
                                 }
