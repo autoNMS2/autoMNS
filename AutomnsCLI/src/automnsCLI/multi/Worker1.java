f31f6061a51508006280f262482d397af2e37bcb
sulalabsi <83693029+sulalabsi@users.noreply.github.com>
2021-10-15 14:34:51 +1100

    fix

--- AutomnsCLI/src/automnsCLI/multi/Worker1.java
+++ AutomnsCLI/src/automnsCLI/multi/Worker1.java
@@ -7,7 +7,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.BufferedReader;
 import java.io.FileWriter;
-import java.util.Scanner;
 
 public class Worker1 extends Agent {
     protected void setup() {
@@ -29,30 +28,24 @@ public class Worker1 extends Agent {
                             break;
                         //Agent Service Status
                         case "Service Update":
+
                             String cmd = "sudo docker ps";
-                            String ps = null;
                             try {
                                 Process proc = r.exec(cmd);
                                 BufferedReader stdInput = new BufferedReader(new
                                         InputStreamReader(proc.getInputStream()));
-
                                 BufferedReader stdError = new BufferedReader(new
                                         InputStreamReader(proc.getErrorStream()));
 
                                 // Read the output from the command
-                                System.out.println("Here is the standard output of the command:\n");
                                 String s = null;
                                 while ((s = stdInput.readLine()) != null) {
-                                    System.out.println(s);
-                                    FileWriter log=new FileWriter("log.txt");
-                                    log.write(s);
-                                    System.out.println("Writing successful");
-                                    //close the file
-                                    log.close();
+                                    ACLMessage reply2 = msg.createReply();
+                                    reply2.setPerformative(ACLMessage.INFORM);
+                                    reply2.setContent("\n" + myAgent.getLocalName() + "\n" + s);
+                                    send(reply2);
                                 }
-
                                 // Read any errors from the attempted command
-                                System.out.println("Here is the standard error of the command (if any):\n");
                                 while ((s = stdError.readLine()) != null) {
                                     System.out.println(s);
                                 }
