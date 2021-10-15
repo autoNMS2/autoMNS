32ccfe950f6b8f8889fe98d0691841bbf69d850d
sulalabsi <83693029+sulalabsi@users.noreply.github.com>
2021-10-15 16:52:42 +1100

    copy folder

--- AutomnsCLI2/src/automnsCLI/multi/Worker1.java
+++ AutomnsCLI2/src/automnsCLI/multi/Worker1.java
@@ -7,6 +7,9 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.BufferedReader;
 import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.io.File;
+import java.util.Scanner;
 
 public class Worker1 extends Agent {
     protected void setup() {
@@ -28,27 +31,29 @@ public class Worker1 extends Agent {
                             break;
                         //Agent Service Status
                         case "Service Update":
-
                             String cmd = "sudo docker ps";
                             try {
                                 Process proc = r.exec(cmd);
                                 BufferedReader stdInput = new BufferedReader(new
                                         InputStreamReader(proc.getInputStream()));
-                                BufferedReader stdError = new BufferedReader(new
-                                        InputStreamReader(proc.getErrorStream()));
+                               File log = new File("log.txt");
+                               FileWriter fw = new FileWriter(log);
+                               PrintWriter pw = new PrintWriter(fw);
+                               String s = null;
+                               while ((s = stdInput.readLine()) != null){
+                                   pw.println(s);
+                               }
+                               pw.close();
 
-                                // Read the output from the command
-                                String s = null;
-                                while ((s = stdInput.readLine()) != null) {
-                                    ACLMessage reply2 = msg.createReply();
-                                    reply2.setPerformative(ACLMessage.INFORM);
-                                    reply2.setContent("\n" + myAgent.getLocalName() + "\n" + s);
-                                    send(reply2);
-                                }
-                                // Read any errors from the attempted command
-                                while ((s = stdError.readLine()) != null) {
-                                    System.out.println(s);
-                                }
+                               Scanner scan = new Scanner(log);
+                               String logContent = null;
+                               while (scan.hasNextLine()){
+                                   logContent = logContent.concat(scan.nextLine() + "\n");
+                               }
+                                ACLMessage reply2 = msg.createReply();
+                                reply2.setPerformative(ACLMessage.INFORM);
+                                reply2.setContent("\n" + myAgent.getLocalName() + "\n" + logContent);
+                                send(reply2);
                             } catch (IOException e)
                             {
                                 e.printStackTrace();
