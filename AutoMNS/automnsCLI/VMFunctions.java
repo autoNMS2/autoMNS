db0cd05240cbe487b63e0e1285ec6f14625244f7
Jamesanthyoung1 <62122658+Jamesanthyoung1@users.noreply.github.com>
2021-10-22 20:51:25 +1100

    Added jar file working for earlier jre

--- AutoMNS/automnsCLI/VMFunctions.java
+++ AutoMNS/automnsCLI/VMFunctions.java
@@ -252,8 +252,8 @@ public class VMFunctions {
 		//The following array contains the commands necessary to initialise the coordinator agent on the first vm provided by the user
 		String[] agentCommands =
 				{
-				"javac -cp autoMNS/AutoMNS/lib/jade.jar:autoMNS/AutoMNS/lib/jsch-0.1.55.jar -d classes autoMNS/AutoMNS/automnsCLI/*.java",
-				"java -cp autoMNS/AutoMNS/lib/jade.jar:autoMNS/AutoMNS/lib/jsch-0.1.55.jar:classes jade.Boot -host " + getVMPrivateIps().get(0) + " -port 1099 -agents 'coordinator:automnsCLI.coordinator("};
+				"javac -cp autoMNS/AutoMNS/lib/jade.jar:autoMNS/AutoMNS/lib/jsch-0.1.55.jar:autoMNS/AutoMNS/lib/ascii-table-1.1.0.jar -d classes autoMNS/AutoMNS/automnsCLI/*.java",
+				"java -cp autoMNS/AutoMNS/lib/jade.jar:autoMNS/AutoMNS/lib/ascii-table-1.1.0.jar:autoMNS/AutoMNS/lib/jsch-0.1.55.jar:classes jade.Boot -host " + getVMPrivateIps().get(0) + " -port 1099 -agents 'coordinator:automnsCLI.coordinator("};
 		/*
 		the following for loops adds the users public and private vm ips as arguments on to the end of the java command
 		this allows the coordinator agent to access these ip addresses
