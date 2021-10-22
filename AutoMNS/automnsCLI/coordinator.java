5b2162f8cb4abf5ad50a6ee194a7421ad98d7fe6
sulalabsi <83693029+sulalabsi@users.noreply.github.com>
2021-10-22 16:11:19 +1100

    apt

--- AutoMNS/automnsCLI/coordinator.java
+++ AutoMNS/automnsCLI/coordinator.java
@@ -29,7 +29,7 @@ public class coordinator extends Agent {
 		//Defining the path to the AWS key
 		Object[] VMs = getArguments();
 		//Defining the path to the AWS key
-		String privateKey = (String) VMs[13];
+		String privateKey = (String) VMs[VMs.length-1];
 		System.out.println(privateKey);
 		//Defining the IP address of the main platform for other agents to join
 		List<String> workerVMsPrivate = new ArrayList<String>();
